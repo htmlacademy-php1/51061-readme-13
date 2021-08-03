39f0610fba0b5f5ef7faab8f9d31ffeadffa36c9
gladkoff <agladkov@logrocon.com>
2021-08-03 22:59:01 +0300

    add valid array keys and apply extract

--- index.php
+++ index.php
@@ -3,49 +3,47 @@
 require_once('helpers.php');
 $is_auth = rand(0, 1);
 
-$user_name = 'Aндрей'; // укажите здесь ваше имя
-
 $posts = [
     [
         'title' => 'Цитата',
         "type" => 'post-quote',
         "content" => 'Мы в жизни любим только раз, а после ищем лишь похожих',
-        "user-name" => 'Лариса',
+        "user_name" => 'Лариса',
         "avatar" => 'userpic-larisa-small.jpg',
     ],
     [
         "title" => 'Игра престолов',
         "type" => 'post-text',
         "content" => 'Не могу дождаться начала финального сезона своего любимого сериала!',
-        "user-name" => 'Владик',
+        "user_name" => 'Владик',
         "avatar" => 'userpic.jpg',
     ],
     [
         "title" => 'Тест',
         "type" => 'post-text',
         "content" => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore ut sunt voluptatibus neque magnam odio sint, obcaecati non facilis enim et aut quisquam explicabo, necessitatibus in. Voluptate aspernatur quidem suscipit assumenda, animi perspiciatis eaque doloremque odit placeat obcaecati temporibus sunt architecto eligendi earum doloribus, ipsa libero, quasi esse asperiores quaerat! Fuga sed dolores voluptate cumque, laboriosam dolor, quae corporis iste in dolorem quam quisquam omnis soluta harum deleniti! Quos magni voluptas velit, porro sit praesentium vel ratione fugit repellat laboriosam eos id cumque officia quibusdam perferendis ea aliquam mollitia aut totam accusamus vero voluptate consequuntur architecto. Harum, commodi in! Ullam iure earum laboriosam minus tempora alias doloribus dicta reprehenderit? Veritatis est minima ipsam eum dolore voluptatem maxime ratione, sed delectus totam unde fugit ipsum cumque laboriosam dicta tempora atque perferendis id adipisci cupiditate optio eos modi vel mollitia. Laudantium quam saepe aliquam ad voluptates inventore soluta nulla dolores nobis! Recusandae optio pariatur at ipsam itaque exercitationem, voluptatibus neque voluptates repellendus odit. Velit amet sed tenetur saepe aut quod cupiditate eos perspiciatis aspernatur laudantium minus, officiis magni. Eius, dignissimos? Nemo, nihil sit? Quisquam accusantium corrupti nihil aliquam, deleniti quasi velit, distinctio doloribus, asperiores hic neque quam explicabo ea. Obcaecati harum, aperiam debitis fuga placeat a non nesciunt nisi ratione esse cum quis! Quaerat omnis harum officia. Fuga voluptatum minima esse tempora quae hic totam eaque quaerat non impedit, excepturi voluptates, architecto nihil vitae necessitatibus ex quisquam repudiandae tenetur neque! Dolorum ullam consequatur corporis atque nobis illo illum voluptas ducimus ab, iusto inventore nostrum ex commodi officia sint? Tempore veniam dolorem, ducimus non dolores fugit distinctio esse corporis laboriosam ex ullam quis fuga aliquam alias porro nesciunt! Soluta culpa consectetur quisquam aspernatur aliquam illum dolor rerum numquam facere libero labore, dolores voluptatum vel deleniti, veniam id aperiam eius provident expedita ab minima.',
-        "user-name" => 'Лукавый',
+        "user_name" => 'Лукавый',
         "avatar" => 'userpic.jpg',
     ],
     [
         "title" => 'Наконец, обработал фотки!',
         "type" => 'post-photo',
         "content" => 'rock-medium.jpg',
-        "user-name" => 'Виктор',
+        "user_name" => 'Виктор',
         "avatar" => 'userpic-mark.jpg',
     ],
     [
         "title" => 'Моя мечта',
         "type" => 'post-photo',
         "content" => 'coast-medium.jpg',
-        "user-name" => 'Лариса',
+        "user_name" => 'Лариса',
         "avatar" => 'userpic-larisa-small.jpg',
     ],
     [
         "title" => 'Лучшие курсы',
         "type" => 'post-link',
         "content" => 'www.htmlacademy.ru',
-        "user-name" => 'Владик',
+        "user_name" => 'Владик',
         "avatar" => 'userpic.jpg',
     ]
 ];
@@ -343,15 +341,7 @@ $posts = [
             <?php
             foreach ($posts as $post): ?>
                 <?php
-                $content = $post['content']; ?>
-                <?php
-                $title = $post['title']; ?>
-                <?php
-                $type = $post['type']; ?>
-                <?php
-                $avatar = $post['avatar']; ?>
-                <?php
-                $user_name = $post['user-name']; ?>
+                extract($post); ?>
                 <article class="popular__post post <?= $type ?>">
                     <header class="post__header">
                         <h2><?= $title ?></h2>
