edb1808e0581a2d0f08a8e0b8a7aa07797231191
gladkoff <agladkov@logrocon.com>
2021-08-29 22:07:28 +0300

    add sql scheme

---
+++ sсhema.sql
@@ -0,0 +1,157 @@
+CREATE
+DATABASE IF NOT EXISTS readme
+	DEFAULT CHARACTER SET utf8;
+USE
+readme;
+
+/**
+Пользователь
+Представляет зарегистрированного пользователя.
+Поля:
+дата регистрации: дата и время, когда этот пользователь завёл аккаунт;
+email;
+логин;
+пароль: хэшированный пароль пользователя;
+аватар: ссылка на загруженный аватар пользователя;
+*/
+CREATE TABLE user
+(
+  id           INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  email        VARCHAR(128) NOT NULL,
+  login        VARCHAR(128) NOT NULL,
+  password     CHAR(64)     NOT NULL,
+  register_day TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  avatar_url   VARCHAR(2048)
+);
+
+/**
+Сообщение
+Одно сообщение из внутренней переписки пользователей на сайте
+Поля:
+дата создания: дата и время, когда это сообщение написали;
+содержимое: задаётся пользователем.
+Связи:
+отправитель: пользователь, отправивший сообщение;
+получатель: пользователь, которому отправили сообщение.
+*/
+CREATE TABLE message
+(
+  id           INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  create_date  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  content      TEXT NOT NULL,
+  sender_id    INT,
+  recipient_id INT,
+  FOREIGN KEY (sender_id) REFERENCES user (id),
+  FOREIGN KEY (recipient_id) REFERENCES user (id)
+);
+/**
+Тип контента
+Один из пяти предопределенных типов контента.
+Поля:
+название (Текст, Цитата, Картинка, Видео, Ссылка);
+имя класса для иконки (photo, video, text, quote, link);
+*/
+CREATE TABLE types
+(
+  id         INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  icon_class TEXT NOT NULL,
+  title      TEXT NOT NULL,
+  CONSTRAINT check_icon_class CHECK
+    (icon_class IN ('photo', 'video', 'text', 'quote', 'link')),
+  CONSTRAINT check_title CHECK
+    (title IN ('текст', 'цитата', 'картинка', 'видео', 'ссылка'))
+);
+/**
+Пост
+Состоит из заголовка и содержимого. Набор полей, которые будут заполнены, зависит от выбранного типа.
+Поля:
+дата создания: дата и время, когда этот пост был создан пользователем;
+заголовок: задаётся пользователем;
+текстовое содержимое: задаётся пользователем;
+автор цитаты: задаётся пользователем;
+изображение: ссылка на сохранённый файл изображения;
+видео: ссылка на видео с youtube;
+ссылка: ссылка на сайт, задаётся пользователем;
+число просмотров.
+Связи:
+автор: пользователь, создавший пост;
+тип контента: тип контента, к которому относится пост;
+хештеги: связь вида «многие-ко-многим» с сущностью «хештег».
+*/
+CREATE TABLE posts
+(
+  id              INT AUTO_INCREMENT PRIMARY KEY,
+  create_date     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  title           TEXT NOT NULL,
+  text            TEXT,
+  img_url         TEXT,
+  video_url       TEXT,
+  link            TEXT,
+  views           INT       DEFAULT 0,
+  author_id       INT,
+  content_type_id INT,
+  FOREIGN KEY (author_id) REFERENCES user (id),
+  FOREIGN KEY (content_type_id) REFERENCES types (id) # hashtag INT - мы же должы хранить тут массив пока не понял как это сделать,
+);
+/**
+Комментарий
+Текстовый комментарий, оставленный к одному из постов.
+Поля:
+дата создания: дата и время создания комментария;
+содержимое: задается пользователем.
+Связи:
+автор: пользователь, создавший пост;
+пост: пост, к которому добавлен комментарий.
+*/
+CREATE TABLE comments
+(
+  id          INT AUTO_INCREMENT PRIMARY KEY,
+  create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  content     TEXT,
+  author_id   INT,
+  post_id     INT,
+  FOREIGN KEY (author_id) REFERENCES user (id),
+  FOREIGN KEY (post_id) REFERENCES posts (id)
+);
+/**
+Лайк
+сущность состоит только из связей и не имеет собственных полей.
+Связи:
+пользователь: кто оставил этот лайк;
+пост: какой пост лайкнули.
+*/
+CREATE TABLE likes
+(
+  id        INT AUTO_INCREMENT PRIMARY KEY,
+  author_id INT,
+  post_id   INT,
+  FOREIGN KEY (author_id) REFERENCES user (id),
+  FOREIGN KEY (post_id) REFERENCES posts (id)
+);
+/**
+Подписка
+Эта сущность состоит только из связей и не имеет собственных полей. Сущность создается, когда пользователь подписывается на другого пользователя.
+Связи:
+автор: пользователь, который подписался;
+подписка: пользователь, на которого подписались
+*/
+CREATE TABLE subscription
+(
+  id           INT AUTO_INCREMENT PRIMARY KEY,
+  author_id    INT,
+  subscription INT,
+  FOREIGN KEY (author_id) REFERENCES user (id),
+  FOREIGN KEY (subscription) REFERENCES user (id)
+);
+/**
+Хештег
+Один из используемых хештегов на сайте. Сущность состоит только из названия хештега.
+*/
+CREATE TABLE hashtag
+(
+  id      INT AUTO_INCREMENT PRIMARY KEY,
+  name    TEXT,
+  post_id INT,
+  FOREIGN KEY (post_id) REFERENCES posts (id)
+);
+
